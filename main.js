9b2eacb8b4efccf8d4763aabb218a3342ba8bfb0
emifretes <emiacsm@gmail.com>
2023-07-05 23:40:56 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+function calcularNotaFinal(dato1, dato2, dato3) {
+    let suma = dato1 + dato2 + dato3;
+    let promedio = suma / 3;
+    
+    let calificacion = '';
+    
+    if (promedio >= 9) {
+        calificacion = 'A';
+    } else if (promedio >= 8) {
+        calificacion = 'B';
+    } else if (promedio >= 7) {
+        calificacion = 'C';
+    } else if (promedio >= 6) {
+        calificacion = 'D';
+    } else {
+        calificacion = 'F';
+    }
+    
+    return calificacion;
+}
+
+    let dato1, dato2, dato3;
+    let elDato = false;
+
+while (!elDato) {
+    dato1 = Number(prompt("Ingrese la nota 1"));
+    dato2 = Number(prompt("Ingrese la nota 2"));
+    dato3 = Number(prompt("Ingrese la nota 3"));
+
+    if (dato1 >= 0 && dato2 >= 0 && dato3 >= 0) {
+    validInput = true;
+    } else {
+    alert("Las notas deben ser valores numéricos no negativos. Inténtelo nuevamente.");
+    }
+}
+
+console.log('La calificación final es: ' + calcularNotaFinal(dato1, dato2, dato3));
